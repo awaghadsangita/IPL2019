dd61553188c4aa50b343eb212986489a1086659a
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-14 11:48:01 +0530

    ADDED:testcase for incorrect delimitor

--- src/test/java/ipl/MostRunsFactSheetTestCase.java
+++ src/test/java/ipl/MostRunsFactSheetTestCase.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 public class MostRunsFactSheetTestCase {
     private static final String RUNS_FACTSHEET = "/home/admin1/IdeaProjects/IPL2019/src/test/resources/IPL2019FactsheetMostRuns.csv";
-    private static final String WRONG_RUNS_FACTSHEET = "/home/admin1/IdeaProjects/IPL2019/src/test/resources/Wrong_csvFile.csv";;
+    private static final String WRONG_RUNS_FACTSHEET = "/home/admin1/IdeaProjects/IPL2019/src/test/resources/Wrong_csvFile.csv";
+    ;
+    private static final String INCORRECT_DELIMITER_RUNS_FACTSHEET = "/home/admin1/IdeaProjects/IPL2019/src/test/resources/IncorrectDelimiter_IPL2019FactsheetMostRuns.csv";
 
     @Test
     public void givenMostRunsFactSheet_ReturnExactNumberOfPlayers() {
@@ -22,6 +24,16 @@ public class MostRunsFactSheetTestCase {
         try {
             IplAnalyser iplAnalyser = new IplAnalyser();
             iplAnalyser.loadMostRunsFactSheet(WRONG_RUNS_FACTSHEET);
+        } catch (IplAnalyserException e) {
+            Assert.assertEquals(IplAnalyserException.ExceptionType.HEADER_CAPTURING_ISSUE, e.type);
+        }
+    }
+
+    @Test
+    public void givenMostRunsFactSheet_ButWithFileWithIncorrectDelimiter_ShouldThrowException() {
+        try {
+            IplAnalyser iplAnalyser = new IplAnalyser();
+            iplAnalyser.loadMostRunsFactSheet(INCORRECT_DELIMITER_RUNS_FACTSHEET);
         } catch (IplAnalyserException e) {
             Assert.assertEquals(IplAnalyserException.ExceptionType.HEADER_CAPTURING_ISSUE, e.type);
         }
