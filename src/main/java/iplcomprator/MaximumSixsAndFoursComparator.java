b995bcbee882a499abd3cb62c3a861d496d03c41
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-14 18:50:18 +0530

    ADDED:created custom comparator for Maximum sixs and fours

--- src/main/java/iplcomprator/MaximumSixsAndFoursComparator.java
+++ src/main/java/iplcomprator/MaximumSixsAndFoursComparator.java
@@ -1,4 +1,12 @@
 package iplcomprator;
 
-public class MaximumSixsAndFoursComparator {
+import ipl.IplDAO;
+
+import java.util.Comparator;
+
+public class MaximumSixsAndFoursComparator implements Comparator<IplDAO> {
+    @Override
+    public int compare(IplDAO iblDaoObject1, IplDAO iblDaoObject2) {
+        return ((iblDaoObject1.sixes*6)+(iblDaoObject1.fours*4)) - ((iblDaoObject2.sixes*6)+(iblDaoObject2.fours*4));
+    }
 }
