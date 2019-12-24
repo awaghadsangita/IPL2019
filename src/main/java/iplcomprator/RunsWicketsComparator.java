cabf1c96096a14c419b261a484c5d634e2bbcbf5
singhprince0088 <singhprince0088@gmail.com>
2019-12-24 11:41:24 +0530

    REFACTORED:change logic of comparator

--- src/main/java/iplcomprator/RunsWicketsComparator.java
+++ src/main/java/iplcomprator/RunsWicketsComparator.java
@@ -1,4 +1,12 @@
 package iplcomprator;
 
-public class RunsWicketsComparator {
+import ipl.IplDao;
+
+import java.util.Comparator;
+
+public class RunsWicketsComparator implements Comparator<IplDao> {
+    @Override
+    public int compare(IplDao iplDao1, IplDao iplDao2) {
+        return (iplDao1.wickets+iplDao1.totalRuns)-(iplDao2.wickets+iplDao2.totalRuns);
+    }
 }
